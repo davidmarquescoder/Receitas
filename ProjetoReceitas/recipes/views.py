1780c73a5235e145b5e2bac90ae8bd9d5f570c9f
David Marques <davidmarques99@hotmail.com>
2023-08-22 14:11:45 -0300

    Configurações iniciais do App e do Project realizada | Settings.py | recipes/urls.py | project/urls.py | templates/index.html | recipes/views.py

--- ProjetoReceitas/recipes/views.py
+++ ProjetoReceitas/recipes/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    utils = {
+        'title': 'HomePage',
+    }
+
+    return render(request, 'index.html', context=utils)
