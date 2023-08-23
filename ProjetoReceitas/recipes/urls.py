5265bf46ae5c98a3f15ad026aa1016b0fc18c366
David Marques <davidmarques99@hotmail.com>
2023-08-23 14:46:18 -0300

    Criando outra View e Url para a página da receita

--- ProjetoReceitas/recipes/urls.py
+++ ProjetoReceitas/recipes/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from recipes.views import home
+from recipes import views
 
 urlpatterns = [
-    path('', home)
+    path('', views.home),
+    # Definimos um parâmetro 'id' para essa url e só vai ser aceito se for um 'inteiro', isso trás mais segurança para o código (URL Dinâmica).
+    path('recipes/<int:id>/', views.recipe),
 ]
--- ProjetoReceitas/recipes/views.py
+++ ProjetoReceitas/recipes/views.py
@@ -8,3 +8,12 @@ def home(request):
     }
 
     return render(request, 'recipes/pages/index.html', context=utils)
+
+# O parâmetro 'id' deve ser obrigatoriamente passado aqui na view.
+def recipe(request, id):
+    utils = {
+        'title': 'Receitas',
+        'nome': 'David Marques',
+    }
+
+    return render(request, 'recipes/pages/index.html', context=utils)
