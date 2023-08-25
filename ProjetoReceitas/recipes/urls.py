e6325163e9f94f5df7160231877360a7fc0cc71d
David Marques <davidmarques99@hotmail.com>
2023-08-25 16:02:28 -0300

    Adicionando nomes para minhas urls, e adicionando links no titulo da receita, na foto e link dinâmico no botão, além de estar gerando um número aleatório para passar como 'id' por enquanto, implementamos também um 'app_name' para as urls

--- ProjetoReceitas/recipes/urls.py
+++ ProjetoReceitas/recipes/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from recipes import views
 
+# Utilizando app name para passar os nomes das urls no html, para não correr o risco de colisão de nomes
+app_name = 'recipes'
+
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name='homepage'),
     # Definimos um parâmetro 'id' para essa url e só vai ser aceito se for um 'inteiro', isso trás mais segurança para o código (URL Dinâmica).
-    path('recipes/<int:id>/', views.recipe),
+    path('recipes/<int:id>/', views.recipe, name='view-recipe'),
 ]
